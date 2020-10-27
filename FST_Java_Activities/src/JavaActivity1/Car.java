f94faef4830c79ac9213d5610763d84a6b39c469
Gowthamn222 <70248433+Gowthamn222@users.noreply.github.com>
2020-10-27 00:59:55 -0500

    Update Car.java

--- FST_Java_Activities/src/JavaActivity1/Car.java
+++ FST_Java_Activities/src/JavaActivity1/Car.java
@@ -2,20 +2,20 @@ package JavaActivity1;
 
 public class Car {
 
-	// Class Member Variables
+	
 	String color;
 	int make;
 	String transmission;
 	int tyres;
 	int doors;
 
-	// Constructor
+	
 	Car() {
 		tyres = 4;
 		doors = 4;
 	}
 
-	// Class Methods
+	
 	public void displayCharacterstics() {
 		System.out.println("Color of the Car: " + color);
 		System.out.println("Make of the Car: " + make);
